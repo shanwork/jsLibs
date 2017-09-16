ae1cdde4a8869a40fa61c77d8ba72a6cef299f41
shanwork <shanwork2012@gmail.com>
2017-09-16 13:29:44 -0700

    Shiv puran adding notes contruct using href

--- d3_and_others/storyBoard/shivji/shivpuran1/scripts/app.js
+++ d3_and_others/storyBoard/shivji/shivpuran1/scripts/app.js
@@ -188,7 +188,11 @@ loadChapterDiv = function(index, postBack=true,static=true){
             subTitleList.push( currentChapter.subsections[i].subTitle);
         }
         if (currentChapter.subsections[i].content && currentChapter.subsections[i].content != '') {
-            contentText +=   currentChapter.subsections[i].content  ;
+              contentText +=   currentChapter.subsections[i].content.replace(/\<note\>\(/g,'\<sup\><a href=\'#').replace(/\)<\/note\>/g,'\'\>note\<\/a\><\/sup\>');//\'\>*</a>\<\sup\>');//.replace(/\)<\/note>,'\\'>*\<\\a\><\/sup\>')  ;
+       //       contentText +=   currentChapter.subsections[i].content.replace(/\<note\>\(/g,'\<sup\><a href=\'#\'\>*</a>\<\sup\>');//.replace(/\)<\/note>,'\\'>*\<\\a\><\/sup\>')  ;
+       // workimg  contentText +=   currentChapter.subsections[i].content.replace(/\<note\>\(.\)<\/note>/g,'\<sup\>A<\/sup\>')  ;
+       //              contentText +=   currentChapter.subsections[i].content.replace(/\<note\>\(.\)\<\\note\>/,'<sup>t</sup>')  ;
+    
         }
     }//.. loading content
     if (subTitleList.length > 1){
